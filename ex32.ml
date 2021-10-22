7a7db75c62c7b873968ceaf8ef510da9361b42c8
Matt Umut Ucak <umutucak79@gmail.com>
2021-10-22 11:49:22 +0200

    tail recursion implemented

--- ex32.ml
+++ ex32.ml
@@ -1,12 +1,13 @@
-let rec fact n =
-	if n = 0 then
-		1
-	else
-		n * fact(n-1)
+let fact n =
+	let rec tail n acc =
+		if n = 0 then
+			acc
+		else
+			tail (n-1) n*acc in tail n 1
 
 let _ =
-	let x = Scanf.scanf "%d" (fun x -> x) in
+	let x = read_int () in
 
 	let y = fact x in
 
-	let _ = Printf.printf "%d\n" y in ()
+	Printf.printf "%d\n" y
