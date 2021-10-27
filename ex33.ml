4c91ce3cd13c72d8a159a7b1b61a706bbfe10ac9
Matt Umut Ucak <umutucak79@gmail.com>
2021-10-27 11:50:40 +0200

    ex33 finished

---
+++ ex33.ml
@@ -0,0 +1,13 @@
+let power x n =
+  let rec tail x n acc =
+    if n = 0 then
+      acc
+    else
+      tail x (n-1) (acc*x) in tail x n 1
+
+let _ =
+  let x = read_int () in
+  let y = read_int () in
+  let z = power x y in
+
+  Printf.printf "%d\n" z
