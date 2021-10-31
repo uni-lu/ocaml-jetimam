39db66934350da6dd40ff1292b710f4624ba608d
Matt Umut Ucak <umutucak79@gmail.com>
2021-10-31 20:36:12 +0100

    syntactic sugar added for ex43

--- ex43.ml
+++ ex43.ml
@@ -1,15 +1,16 @@
 let reverse l =
-  let rec tail l acc =
-    match l with
+  let rec tail acc = function
     | [] -> acc
-    | h::t -> tail t (h::acc)
-  in tail l []
+    | h::t -> tail (h::acc) t
+  in tail [] l
 
 let append l1 l2 =
-  let rec loop l1 l2 =
-    match l2 with
+  let rec loop l1 = function
     | [] -> l1
     | h::t -> loop (h::l1) t
     in loop l1 l2
 
-let _ = List.iter (Printf.printf "%d ") (append (reverse [1;2;3]) [4;5;6])
+let _ = 
+  let l1 = [1;2;3] in
+  let l2 = [4;5;6] in
+  List.iter (Printf.printf "%d ") (append (reverse l1) l2)
