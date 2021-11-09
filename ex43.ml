e7b2a9888212cfc392f518987063065a7121a846
Matt Umut Ucak <umutucak79@gmail.com>
2021-11-09 21:13:59 +0100

    code enhanced

--- ex43.ml
+++ ex43.ml
@@ -6,11 +6,11 @@ let reverse l =
 
 let append l1 l2 =
   let rec loop l1 = function
-    | [] -> l1
+    | [] -> reverse l1
     | h::t -> loop (h::l1) t
     in loop l1 l2
 
 let _ = 
   let l1 = [1;2;3] in
   let l2 = [4;5;6] in
-  List.iter (Printf.printf "%d ") (reverse (append (reverse l1) l2))
+  List.iter (Printf.printf "%d ") (append (reverse l1) l2))
